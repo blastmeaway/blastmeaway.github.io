ae8d46300a171560b1e0ec95e8026073ff6f3445
blastmeaway <57377796+blastmeaway@users.noreply.github.com>
2019-11-06 11:45:30 -0800

    Run 2
    With HTML Index

--- STHSSetting.php
+++
@@ -1,6 +0,0 @@
-﻿<?php
-$DatabaseFile = (string)"CELH-STHS.db";
-$CareerStatDatabaseFile = (string)"CELH-STHSCareerStat.db";
-$NewsDatabaseFile = (string)"CELH-STHSNews.db";
-$WebClientHeadCode = "<link href=\"STHSMain.css\" rel=\"stylesheet\" type=\"text/css\" /><link href=\"CELH-PLF.css\" rel=\"stylesheet\" type=\"text/css\" /><script type=\"text/javascript\" src=\"JS/STHSWebEditorMenu.js\"></script>";
-?>
